13200954e20b3b22a02373a7fd1976de32893948
AmirHossein Ghasemzadeh <a.ghasemzadeh@farir.local>
2023-04-01 13:56:11 +0430

    add head and layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import Navbar from './components/Navbar';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 
 export default function Home() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <main>
-          <Header />
-          <RestaurantCard />
-        </main>
-      </main>
+    <main>
+      <Header />
+      <RestaurantCard />
     </main>
   );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,3 @@
-import Header from '@/app/components/Header';
-import Navbar from '@/app/components/Navbar';
-import Link from 'next/link';
 import RestaurantNavbar from './components/RestaurantNavbar';
 import Title from './components/Title';
 import Description from './components/Description';
@@ -11,25 +8,19 @@ import ReservationCard from './components/ReservationCard';
 
 const RestaurantDetails = () => {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <Header />
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[70%] rounded p-3 shadow'>
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className='w-[27%] relative text-reg'>
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className='w-[27%] relative text-reg'>
+        <ReservationCard />
+      </div>
+    </>
   );
 };
 
